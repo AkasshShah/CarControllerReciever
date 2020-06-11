61637f06e7e36df3132abd313905f58edaaa7fb8
unknown <akassh.shah77@gmail.com>
2020-06-11 16:59:40 -0400

    run from anywhere

--- runForever.sh
+++ runForever.sh
@@ -1,6 +1,6 @@
-MYDIR = "$(dirname "$(which "$0")")"
-PATHTOSCRIPT = "${MYDIR}/main.py"
-echo "$PATHTOSCRIPT"
+$MYDIR = "$(dirname "$(which "$0")")"
+$PATHTOSCRIPT = "${MYDIR}/main.py"
+echo "${PATHTOSCRIPT}"
 while true ; do
     /usr/bin/python3 $PATHTOSCRIPT
 done
