cdcbf18b092c837819b240854103830953d4519d
unknown <akassh.shah77@gmail.com>
2020-06-11 23:59:23 -0400

    make it show pid

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import HandleGPIO as GP
 
 if __name__ == "__main__":
     print(sys.argv)
-    with open(".pids", "w") as file1:
+    with open("pids.txt", "w") as file1:
         L = ["bash pid: " + str(sys.argv[-1]), "\npyth pid: " + str(os.getpid())]
         file1.writelines(L)
     print("starting at", SS.ServerIP, "on port", SS.ServerPort, "pid: ", os.getpid())
