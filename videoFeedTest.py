6ab7d3d8b2eafbc794657b7ce5edfb5fedf61d2f
unknown <akassh.shah77@gmail.com>
2020-06-19 17:39:33 -0400

    Video testing phase start

--- videoFeedTest.py
+++ videoFeedTest.py
@@ -47,13 +47,13 @@ class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 def main():
 	global capture
 	capture = cv2.VideoCapture(0)
-	capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320); 
-	capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240);
-	capture.set(cv2.cv.CV_CAP_PROP_SATURATION,0.2);
+	capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 320) 
+	capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 240)
+	capture.set(cv2.cv.CV_CAP_PROP_SATURATION,0.2)
 	global img
 	try:
 		server = ThreadedHTTPServer(('localhost', 8080), CamHandler)
-		print "server started"
+		print("server started")
 		server.serve_forever()
 	except KeyboardInterrupt:
 		capture.release()
