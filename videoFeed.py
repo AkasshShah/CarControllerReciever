f562af253e7f119cc84a30b28b54633451a8670e
unknown <akassh.shah77@gmail.com>
2020-06-26 21:47:30 -0400

    testing video feed

--- videoFeed.py
+++ videoFeed.py
@@ -70,7 +70,7 @@ output = StreamingOutput()
 def startCam(height=640, width=480, frameRate=60, rotation=0):
     global PAGE
     reso = str(height) + "x" + str(width)
-    PAGE = "<html><head><title>Raspi Cam</title></head><body><center><h1>Raspi Cam</h1></center><center><img src=\"stream.mjpg\" width=\"" + width + "\" height=\"" + height + "\"></center></body></html>"
+    PAGE = "<html><head><title>Raspi Cam</title></head><body><center><h1>Raspi Cam</h1></center><center><img src=\"stream.mjpg\" width=\"" + str(width) + "\" height=\"" + str(height) + "\"></center></body></html>"
     camera = picamera.PiCamera(resolution=reso, framerate=frameRate)
     camera.rotation = rotation
     camera.start_recording(output, format='mjpeg')
